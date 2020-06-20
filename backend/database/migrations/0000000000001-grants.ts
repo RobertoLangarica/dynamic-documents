0b8bc23ac35fa366bcfa62484a9eb2ed3f4b1b12
Roberto <roberto@villavanilla.com>
2020-06-20 10:09:59 -0500

    - Removing the extension creation from migrations since their need a super user (the start_db.sh script contains extension addition and could be used in replacement)

--- backend/database/migrations/0000000000001-grants.ts
+++ backend/database/migrations/0000000000001-grants.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner, Table} from 'typeorm'
 export class grants0000000000001 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    // await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
     await queryRunner.createTable(new Table({
       name: 'grants',
