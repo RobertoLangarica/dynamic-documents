450b42ff2cd2351fd01190e464ec4a8f05323067
Roberto <roberto@villavanilla.com>
2020-06-20 10:06:17 -0500

    - start_db.sh is now in charge of creating db's, users and extensions. This allows the deprecation of init_db.sh inside the db image, so now we can use the postgres image.

--- backend/start_db.sh
+++ backend/start_db.sh
@@ -1,75 +1,76 @@
 #!/bin/bash
 
 
-echo "Sarting DB server"
+printf "################################\n#      STARTING DB SERVER      #\n################################\n\n"
 context=$(dirname $BASH_SOURCE)
 exist_credentials=0
 file=$context/.env
-networkName=
+networkName=dd_back_net
 isRunning=
 name=db_server
-imageName=registry.digitalocean.com/wizard/postgres
+# imageName=registry.digitalocean.com/wizard/postgres
+imageName=postgres:12
 
-
+printf "  -> Reading credentilas from .env\n"
+# Reading credentials
 if [ -f $file ]; then
     exist_credentials=1
-    echo "Using credentias FROM: .env"
+    echo
 else
-    echo "There is no .env present"
+    printf "     NOTICE: There is no .env present\n\n"
     exist_credentials=0
 fi
 
-# Network
+if [ $exist_credentials -eq 1 ]; then
+    DB_PASSWORD=$(grep DB_PASSWORD $file | cut -d "=" -f 2)
+    DB_USERNAME=$(grep DB_USERNAME $file | cut -d "=" -f 2)
+    DB_DATABASE=$(grep DB_DATABASE $file | cut -d "=" -f 2)
+fi
+
+POSTGRES_PASSWORD=admin
+POSTGRES_USER=postgres
+POSTGRES_DB=postgres
+PORT=5432
+
+
+# Reading a specific network for communicatong the services
 if [ $# -gt 0 ]; then
     if [ "$1" == -n ]; then
         shift
         if [ "$1" != "" ]; then
             networkName=$1
             shift
+            echo
         else
-            echo "Missing network for -n"
+            printf "  -> ERROR: Missing network for -n \n\n"
             exit 1
         fi
     fi
 fi
 
-# Start the postgres server only if it is not running
-isRunning=$(docker ps -f name=$name | wc -l)
-if [ $isRunning -lt 2 ]; then
-
-    if [ $exist_credentials -eq 1 ]; then
-        DB_PASSWORD=$(grep DB_PASSWORD $file | cut -d "=" -f 2)
-        DB_USERNAME=$(grep DB_USERNAME $file | cut -d "=" -f 2)
-        DB_DATABASE=$(grep DB_DATABASE $file | cut -d "=" -f 2)
-    fi
-
-    POSTGRES_PASSWORD=admin
-    POSTGRES_USER=postgres
-    POSTGRES_DB=postgres
-
+# Check if the postgres image server is already running
+isRunning=$(docker ps -f name=$name | grep -w $name | wc -l)
+if [ $isRunning -ne 1 ]; then
     # Data directory
     PGDATA=/var/lib/postgresql/data/pgdata
 
-    # The network used is ready?
-    if [ "$networkName" != "" ]; then
-        network=$(docker network ls | grep -w $networkName | wc -l)
-
-        if [ $network -eq 0 ]; then
-            echo Creating the service network: $networkName 
-            # Creating the network
-            docker network create $networkName
-        fi
+    # The network needed is ready?
+    network=$(docker network ls | grep -w $networkName | wc -l)
+    if [ $network -eq 0 ]; then
+        printf "  -> CREATING: Docker network $networkName for communication\n\n"
+        # Creating the network
+        docker network create $networkName
     else
-        networkName=dd_back_net
-        echo Using default network: $networkName
+        printf "  -> USING: Docker network $networkName for communication\n\n"
     fi
 
     ########
     # When the DB is deployed in production the restart param should be --restart always
     # When the DB is deployed locally the restart param could be --restart unless-stopped
     ########
+    printf "  -> RUNNING: Docker image $imageName\n\n"
     docker run --name $name -d \
-            -p 5432:5432 \
+            -p $PORT:$PORT \
             -v postgresData:/var/lib/postgresql/data \
             --restart unless-stopped \
             --network $networkName \
@@ -86,11 +87,84 @@ if [ $isRunning -lt 2 ]; then
     # give it some time to start the server
     sleep 5
 else
-    echo PostgresSQL server already running.
+    printf "  -> PostgresSQL image already running. Skipping image initialization\n"
     run=0
 fi
 
-# User and db initilization
+# User, db and extensions initilization
 if [[ $run -eq 0 && $exist_credentials -eq 1 ]]; then
-    docker exec $name /docker-entrypoint-initdb.d/init_db.sh
+    # docker exec $name /docker-entrypoint-initdb.d/init_db.sh
+
+    CONNECTION=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:$PORT/$DB_DATABASE
+
+    # Waiting for the server
+    serverError=2 # 2 is the error returned when the server is down
+    while [ $serverError -eq 2 ]; do
+    psql $CONNECTION -v ON_ERROR_STOP=1 --command "\echo '      Server up...'"
+    serverError=$?
+    sleep 1
+    done
+
+    printf "\n\n--------------------------------\n-> CREATING: Custom user an database\n\n"
+    ######## USER and DATABASE
+    if [[ "$DB_DATABASE" == "" || "$DB_USERNAME" == "" ]]; then
+        echo "   NOTICE: Missing DB_DATABASE or DB_USERNAME --> Skipping creation"
+    else
+        ######## Creating user if not exist
+        user_exist=$(psql $CONNECTION -v ON_ERROR_STOP=1 -c "\du" | grep -w $DB_USERNAME | wc -l)
+        if [ $user_exist -ge 1 ]; then
+            echo "   User $DB_USERNAME already exist --> Skipping creation"
+            user=0
+        else
+            echo " -> Creating USER: $DB_USERNAME"
+            psql $CONNECTION -v ON_ERROR_STOP=1 -q <<-EOSQL 
+            CREATE USER $DB_USERNAME WITH 
+            PASSWORD '$DB_PASSWORD'
+            CREATEDB 
+            CREATEROLE;
+EOSQL
+            user=$?
+        fi
+        #########################################
+
+        ######## Creating database if not exist
+        db_exist=$(psql $CONNECTION -v ON_ERROR_STOP=1 -c "\list" | grep -w $DB_DATABASE | wc -l)
+        if [ $db_exist -ge 1 ]; then
+            echo "   Database $DB_DATABASE already exist --> Skipping creation"
+            db=0
+        else
+            echo " -> Creating DB: $DB_DATABASE"
+            psql $CONNECTION -v ON_ERROR_STOP=1 -q <<-EOSQL
+            CREATE DATABASE $DB_DATABASE;
+            GRANT ALL PRIVILEGES ON DATABASE $DB_DATABASE to $DB_USERNAME
+EOSQL
+            db=$?
+        fi
+        #########################################
+    fi
+
+    printf "\n--------------------------------\n-> ADDING: DB extensions\n"
+    
+    # Usage getExtension $extensions $index
+    # $extensions is a comma separated value
+    # $index starting at 1
+    function getExtension () {
+        echo $(echo $1 | cut -d , -f $2)
+    }
+
+    extensions=uuid-ossp
+    length=1 #Extension count
+    index=1
+    while [ $index -le $length ]
+    do
+        echo extensions[$index]=$(getExtension $extensions $index)
+        extension=$(getExtension $extensions $index)
+        
+        echo " -> Adding: $extension"
+        
+        psql $CONNECTION -v ON_ERROR_STOP=1 -q <<-EOSQL
+        CREATE EXTENSION IF NOT EXISTS "$extension"
+EOSQL
+        index=$((index+1))
+    done
 fi
