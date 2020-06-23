7b659d6960ad7fd60c2a6072124d90114055a374
Roberto <roberto@villavanilla.com>
2020-06-22 20:52:39 -0500

    - Changing the typeORM config file from: config.ts to ormconfig.ts (to match the name used in the typeORM docs)

--- backend/database/ormconfig.ts
+++ backend/database/ormconfig.ts
@@ -12,13 +12,8 @@ const config = (): ConnectionOptions => {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    logging: (process.env.NODE_ENV != 'production'),
-    entities: [
-      User,
-      Token,
-      Grant,
-      APISecret
-    ],
+    logging: false,
+    entities: [User, Token, Grant, APISecret],
     synchronize: false
   }
   console.log(config)
