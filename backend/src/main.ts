d19d3ddaa4dec7281c1bed2b10351c31838c1c80
Roberto <roberto@villavanilla.com>
2020-06-24 16:04:13 -0500

    - Adding CORS with origin:"*" to facilitate the testing.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,15 @@
-import {NestFactory} from '@nestjs/core'
-import {AppModule} from './app.module'
-import {ValidationPipe} from '@nestjs/common'
-import {GlobalExceptionsFilter} from './common/filters/exceptions.filter'
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
-import {EntityNotFoundFilter} from './common/filters/entity-not-found.filter'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { GlobalExceptionsFilter } from './common/filters/exceptions.filter'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { EntityNotFoundFilter } from './common/filters/entity-not-found.filter'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalFilters(
     new GlobalExceptionsFilter(),
     new EntityNotFoundFilter()
@@ -21,6 +22,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('docs', app, document)
 
+  let cors: CorsOptions = { origin: "*" }
+  console.warn("USING: CORS with '*' as origin.")
+
+  app.enableCors(cors)
   await app.listen(3000)
 }
 
