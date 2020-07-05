20f8e0b1388ac98982a204b3a79c2574f59dea22
Roberto <roberto@villavanilla.com>
2020-07-05 13:01:55 -0500

    - Field base code

--- backend/src/templates/field.entity.ts
+++ backend/src/templates/field.entity.ts
@@ -3,16 +3,39 @@ import { Column, Entity } from "typeorm";
 
 @Entity('fields')
 export class Field extends BasicEntity {
+    @Column()
     type: string;
+
+    @Column({ default: () => "" })
     hint: string;
+
+    @Column({ default: () => "" })
     description: string;
+
+    @Column({ default: () => "" })
     name: string;
+
+    @Column()
     editable: boolean;
-    embdeddable: string;
-    embededdable: string;
+
+    @Column()
+    embededdable: boolean; // A field is only embeddable if it has no children
+
+    @Column()
     categories: string[];
+
+    @Column()
     accepted_values: any[];
+
+    @Column()
     validation_expression: string[]
+
     @Column({ nullable: true, default: () => null })
     value: string
+
+    @Column({ nullable: true })
+    default_value: string
+
+    //relacion
+    fields: Field[]
 }
