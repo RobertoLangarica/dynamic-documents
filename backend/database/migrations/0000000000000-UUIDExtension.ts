8a60e4b35da975f93fd1a8ff978209feff1f1c01
Roberto <roberto@villavanilla.com>
2020-07-21 16:38:14 -0500

    - Backend remote build and deploy test

---
+++ backend/database/migrations/0000000000000-UUIDExtension.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class UUIDExtension0000000000000 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP EXTENSION IF EXISTS "uuid-ossp"`);
+  }
+}
