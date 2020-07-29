4ab3e60c4101189860b7a2f6a02dfed4d529b536
Roberto <roberto@villavanilla.com>
2020-07-29 16:06:53 -0500

    - Creating test database

--- backend/test/init_test_db.js
+++ backend/test/init_test_db.js
@@ -1,12 +1,16 @@
 const { Client } = require('pg')
 
-const pgclient = new Client({
+const config = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: 'postgres'
-})
+}
+
+const pgclient = new Client(config)
+
+console.log(config)
 
 pgclient.connect()
 
