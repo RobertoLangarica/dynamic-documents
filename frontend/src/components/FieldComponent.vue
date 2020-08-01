2ddd2294b2df410c87c02ea68b47ee233b40d3d3
Ramses <ramses@cuatromedios.com>
2020-08-01 14:35:26 -0500

    Initial layout and properties changes depending of the state of the document

--- frontend/src/components/FieldComponent.vue
+++ frontend/src/components/FieldComponent.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div class="field-controls flex">
+    <div class="field-controls flex" v-if="currentView === 'edit'">
       <q-btn icon="add" flat round size="md" dense class="cursor-pointer" color="grey" @click="$emit('onShowAddFieldDialog')" />
       <q-btn icon="drag_indicator" flat round size="md" dense class="cursor-drag" color="grey" />
     </div>
     <div class="field-content">
       <q-badge v-if="currentView !== 'capture'" color="secondary"> {{ field.name }}</q-badge>
+      <q-input v-model="text" label="Standard" outlined :readonly="currentView === 'view'" />
     </div>
   </div>
 </template>
@@ -18,5 +19,7 @@ export default class ClassComponent extends Vue {
   @Prop({ type: Object, required: true }) readonly field!: object;
   @Prop({ type: Array, required: true }) readonly fields!: object;
   @Prop({ type: String, required: true }) readonly currentView!: object;
+
+  text = 'Abc'
 }
 </script>
