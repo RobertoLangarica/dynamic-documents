54a124d73877a6e4abccd145b5b54dc31e752454
Roberto <roberto@villavanilla.com>
2020-08-04 17:32:32 -0500

    - Adding IVIews enum to Document

--- frontend/src/pages/Document.vue
+++ frontend/src/pages/Document.vue
@@ -10,10 +10,7 @@
       />
     </div>
     <h1>Doc</h1>
-    <div
-      v-if="docReady"
-      :class="currentView"
-    >
+    <div v-if="docReady" :class="currentView">
       <draggable
         v-model="filteredFields"
         handle=".cursor-drag"
@@ -41,91 +38,116 @@
         @click="showAddFieldDialog"
       />
     </div>
-    <q-spinner
-      v-else
-      color="primary"
-      size="3em"
-      :thickness="2"
-    />
+    <q-spinner v-else color="primary" size="3em" :thickness="2" />
   </article>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import draggable from 'vuedraggable'
-import FieldTypeDialog from 'components/FieldTypeDialog.vue'
-import FieldComponent from 'components/FieldComponent.vue'
-import { DocumentEditionManager } from 'src/dynamic-documents/src/DocumentEditionManager'
+import Vue from "vue";
+import Component from "vue-class-component";
+import draggable from "vuedraggable";
+import FieldTypeDialog from "components/FieldTypeDialog.vue";
+import FieldComponent from "components/FieldComponent.vue";
+import { DocumentEditionManager } from "src/dynamic-documents/src/DocumentEditionManager";
+
+export enum IViews {
+  EDIT,
+  CAPTURE,
+  PRINT,
+}
 
 @Component({ components: { FieldComponent, draggable } })
 export default class Document extends Vue {
-  currentView = 'edit'
-  manager!: DocumentEditionManager
+  currentView: IViews = IViews.EDIT;
+  manager!: DocumentEditionManager;
   views = [
-    { label: 'Editar', value: 'edit' },
-    { label: 'Capturar', value: 'capture' },
-    { label: 'Ver', value: 'view' }
-  ]
+    { label: "Editar", value: IViews.EDIT },
+    { label: "Capturar", value: IViews.CAPTURE },
+    { label: "Ver", value: IViews.PRINT },
+  ];
 
-  docReady = false
-
-  get documentFields () {
-    // TODO: Change this once the store is migrated to TS
-    return this.manager ? this.manager.fields : []
+  get isInEditView() {
+    return this.currentView === IViews.EDIT;
   }
 
-  get filteredFields () {
+  get isInCaptureView() {
+    return this.currentView === IViews.CAPTURE;
+  }
+
+  get isInPrintView() {
+    return this.currentView === IViews.PRINT;
+  }
+
+  // views = [
+  //   { label: "Editar", value: "edit" },
+  //   { label: "Capturar", value: "capture" },
+  //   { label: "Ver", value: "view" },
+  // ];
+
+  docReady = false;
+
+  get documentFields() {
+    // TODO: Change this once the store is migrated to TS
+    return this.manager ? this.manager.fields : [];
+  }
+
+  get filteredFields() {
     return this.manager.fields.filter(
       (f) => f.group_by === "" || f.group_by === undefined
     );
   }
 
-  async mounted () {
-    let document = await this.$store.dispatch('getDocument', this.$route.params.id)
-    await this.$store.dispatch('updateTypes')
-    this.manager = DocumentEditionManager.createFromRemoteObject(document)
-    this.docReady = true
+  async mounted() {
+    let document = await this.$store.dispatch(
+      "getDocument",
+      this.$route.params.id
+    );
+    await this.$store.dispatch("updateTypes");
+    this.manager = DocumentEditionManager.createFromRemoteObject(document);
+    this.docReady = true;
     // TODO: Set these properties with the real data
-    this.manager.isTemplate = false
-    this.manager.isDocument = true
+    this.manager.isTemplate = false;
+    this.manager.isDocument = true;
   }
 
-  showAddFieldDialog () {
-    this.$q.dialog({
-      component: FieldTypeDialog,
-      // optional if you want to have access to
-      // Router, Vuex store, and so on, in your
-      // custom component:
-      parent: this, // becomes child of this Vue node
-      // ("this" points to your Vue component)
-      // props forwarded to component
-      // (everything except "component" and "parent" props above):
-      text: 'something'
-      // ...more.props...
-    }).onOk((type) => {
-      console.log('OK', type)
-    })
+  showAddFieldDialog() {
+    this.$q
+      .dialog({
+        component: FieldTypeDialog,
+        // optional if you want to have access to
+        // Router, Vuex store, and so on, in your
+        // custom component:
+        parent: this, // becomes child of this Vue node
+        // ("this" points to your Vue component)
+        // props forwarded to component
+        // (everything except "component" and "parent" props above):
+        text: "something",
+        // ...more.props...
+      })
+      .onOk((type) => {
+        console.log("OK", type);
+      });
   }
 }
 </script>
 
 <style lang="scss">
-  .field-container {
-    display: flex;
-    padding: 0.5em;
+.field-container {
+  display: flex;
+  padding: 0.5em;
+  .field-controls {
+    width: 5em;
+    opacity: 0;
+    transition: opacity 0.25s;
+  }
+  .field-content {
+    flex: 1;
+  }
+  &:hover,
+  &:active {
     .field-controls {
-      width: 5em;
-      opacity: 0;
-      transition: opacity 0.25s;
-    }
-    .field-content {
-      flex: 1
-    }
-    &:hover, &:active {
-      .field-controls {
-        opacity: 1;
-      }
+      opacity: 1;
     }
   }
+}
 </style>
