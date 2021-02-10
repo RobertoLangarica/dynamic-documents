548a9935fac6706153be70eae770e34c61d602df
Roberto <roberto@villavanilla.com>
2021-02-09 20:29:03 -0600

    Enhancing the EmbedMixin to avoid repeated code and to get a better communication with the document. Adjusting the embed pages so they communicate in a cleaner way with the document

--- frontend/src/pages/embeds/Edition.vue
+++ frontend/src/pages/embeds/Edition.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <dd-doc ref="doc_creation" :isTemplate="isTemplate" :id="id" @mount_ready="onDocReady" />
+    <dd-doc 
+      ref="doc_creation" 
+      :isTemplate="isTemplate" 
+      :id="id"
+      :views="available_views"
+      />
   </div>
 </template>
 
@@ -8,35 +13,25 @@
 import { Component, Prop } from 'vue-property-decorator'
 import { mixins } from 'vue-class-component'
 import EmbedMixin from './EmbedMixin'
-import Document from 'src/pages/Document'
+import Document from 'src/components/dd/Document'
 
 @Component({ components: { 'dd-doc': Document } })
 export default class Creation extends mixins(EmbedMixin) {
     @Prop({ type: String, required: false, default: '' }) readonly id!: string;
     @Prop({ type: Boolean, required: false, default: false }) readonly isTemplate!: boolean;
-
-    async onMessage (message, data) {
+    
+    async onMessage (message, data, handled = false) {
       switch (message) {
-        case 'get_fields':
-          // Sending back the fields list
-          let fields = this.$refs.doc_creation.getFields()
-          this.sendMessage('set_fields', fields)
-          break;
         case 'create':
           /* Saving */
           let document = await this.$refs.doc_creation.saveAsNew()
           this.sendMessage('created', document)
           break;
         default:
-          console.log(`Unrecognized event->${message}`)
+          if(!handled){
+            console.log(`Unrecognized event->${message}`)
+          }
       }
-    }
-
-    onDocReady () {
-      this.$nextTick(() => {
-        let el = this.$refs.doc_creation.$el
-        this.sendMessage('dd_resize', { width: Math.max(el.scrollWidth, el.offsetWidth), height: Math.max(el.scrollHeight, el.offsetHeight) })
-      })
     }
 }
 </script>
