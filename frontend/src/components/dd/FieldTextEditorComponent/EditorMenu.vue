ea3a00bd53f5699be2f42f6168fcc7e5912edaa9
Ramses <ramses@cuatromedios.com>
2021-02-16 11:49:03 -0600

    Hidden editor toolbar

--- frontend/src/components/dd/FieldTextEditorComponent/EditorMenu.vue
+++ frontend/src/components/dd/FieldTextEditorComponent/EditorMenu.vue
@@ -1,47 +1,47 @@
 <template>
-  <editor-menu-bar class="q-mb-md q-mt-sm menubar" :class="{ 'is-hidden':hidden }" :editor="editor" v-slot="{ commands, isActive }">
-    <div class="row justify-between">
-      <q-btn class="col-1 menubar__button" @click="$emit('embed_field',commands.field_embedded) " size="xs">
+  <editor-menu-bar class="menubar" :class="{ 'is-hidden':hidden }" :editor="editor" v-slot="{ commands, isActive }">
+    <div class="row justify-start">
+      <q-btn flat dense class="col-1 menubar__button" @click="$emit('embed_field',commands.field_embedded) " size="xs">
         <q-icon name="image" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold" size="xs">
         <q-icon name="format_bold" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic" size="xs">
         <q-icon name="format_italic" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike" size="xs">
         <q-icon name="format_strikethrough" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline" size="xs">
         <q-icon name="format_underline" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.paragraph() }" @click="commands.paragraph" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.paragraph() }" @click="commands.paragraph" size="xs">
         <q-icon name="title" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 1 }) }" @click="commands.heading({ level: 1 })" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 1 }) }" @click="commands.heading({ level: 1 })" size="xs">
         H1
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 2 }) }" @click="commands.heading({ level: 2 })" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 2 }) }" @click="commands.heading({ level: 2 })" size="xs">
         H2
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 3 }) }" @click="commands.heading({ level: 3 })" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.heading({ level: 3 }) }" @click="commands.heading({ level: 3 })" size="xs">
         H3
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.bullet_list() }" @click="commands.bullet_list" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.bullet_list() }" @click="commands.bullet_list" size="xs">
         <q-icon name="format_list_bulleted" />
       </q-btn>
 
-      <q-btn class="col-1 menubar__button" :class="{ 'is-active': isActive.ordered_list() }" @click="commands.ordered_list" size="xs">
+      <q-btn flat dense class="col-1 menubar__button" :class="{ 'is-active': isActive.ordered_list() }" @click="commands.ordered_list" size="xs">
         <q-icon name="format_list_numbered" />
       </q-btn>
     </div>
@@ -75,11 +75,14 @@ $color-black:rgb(.3,.3,.3);
 
 .menubar {
     margin-bottom: 1rem;
-    transition: visibility 0.2s 0.4s, opacity 0.2s 0.4s;
+    transition: all 0.2s 0.4s, opacity 0.2s 0.4s;
+    height: 2rem;
+  overflow: hidden;
 
     &.is-hidden {
         visibility: hidden;
         opacity: 0;
+        height: 0rem;
     }
 
     &.is-focused {
