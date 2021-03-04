f890860c5ebdee679be29ce76e3ab8f1e749f34b
Roberto <roberto@villavanilla.com>
2021-03-03 18:20:40 -0600

    -Registering the fillmap fields as global componentes so they can be used in a circular dependency way without problems

--- frontend/src/boot/dynamic_documents.js
+++ frontend/src/boot/dynamic_documents.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import FieldGroupComponent from "src/components/dd/FieldGroupComponent";
 import FieldTextEditorComponent from "src/components/dd/FieldTextEditorComponent/FieldTextEditorComponent";
-
+import FieldDest from "src/components/dd/Fillmap/FieldDest";
+import FieldSrc from "src/components/dd/Fillmap/FieldSrc";
 // we globally register our components
 Vue.component('field-group-component', FieldGroupComponent)
 Vue.component('field-text-editor-component', FieldTextEditorComponent)
+Vue.component('field-dest', FieldDest)
+Vue.component('field-src', FieldSrc)
