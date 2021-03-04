e7cd721bf8dd4a600ae178898169dd34bc3ef6a6
Roberto <roberto@villavanilla.com>
2021-03-03 18:16:33 -0600

    fillmap.service: Adding a response transformer for returning standardized schemas

--- backend/src/fillmaps/fillmap.service.ts
+++ backend/src/fillmaps/fillmap.service.ts
@@ -11,17 +11,25 @@ export class FillmapService {
     ) { }
 
     async findById(id: string): Promise<Fillmap> {
-        return await this.fillmap_repo.findOne(id)
+        return this.transformResponse(await this.fillmap_repo.findOne(id))
     }
 
     async findAll(): Promise<{items:Fillmap[]}> {
-        return { items: await this.fillmap_repo.find() }
+        return { items: this.transformResponse(await this.fillmap_repo.find()) }
     }
 
     async findBy(source:string, destination:string): Promise<{items:Fillmap[]}> {
+        // source-destination
         let where:any = Object.assign({},source ? {source_type:source}:{}, destination ? {destination_type:destination}:{})
-
-        return { items: await this.fillmap_repo.find({where}) }
+        let items = await this.fillmap_repo.find({where})
+        
+        // destination-source
+        if(items.length === 0){
+            where = Object.assign({},source ? {destination_type:source}:{}, destination ? {source_type:destination}:{})
+            let reverseItems = await this.fillmap_repo.find({where})
+            items = items.concat(reverseItems)
+        }
+        return { items: this.transformResponse(items) }
     }
 
     async addFillmap(data: FillmapCreationDto): Promise<Fillmap> {
@@ -31,12 +39,13 @@ export class FillmapService {
             fillmap = await this.fillmap_repo.save(fillmap)
             return fillmap
         } catch (e) {
-                throw new InternalServerErrorException(e.message)
+            throw new InternalServerErrorException(e.message)
         }
     }
 
     async updateFillmap(id: string, data: FillmapDto) {
         data.id = id;
+        console.log(data)
         let fillmap = await this.fillmap_repo.preload(data)
 
         // Avoiding duplicates
@@ -52,10 +61,27 @@ export class FillmapService {
         }
 
         fillmap = await this.fillmap_repo.save(fillmap)
-        return fillmap
+        return this.transformResponse(fillmap)
     }
 
     async deleteFillmap(id: string) {
         await this.fillmap_repo.delete({ id: id })
+    }
+
+    // TODO this should be an interceptor
+    transformResponse(data){
+        if(Array.isArray(data)){
+            return data.map(i=>{
+                let r = Object.assign({},i)
+                delete r.created_at
+                delete r.updated_at
+                return r
+            })
+        }
+
+        let r = Object.assign({},data)
+        delete r.created_at
+        delete r.updated_at
+        return r
     }
 }
