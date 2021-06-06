57ced272482b5e6f9463645e152209076b3f484c
Roberto Langarica <roberto.langarica@gmail.com>
2021-06-05 21:31:44 -0500

    frontend/src/components/dd/FieldComponent.vue: Is now showing the components that allow the fillmap functionality

--- frontend/src/components/dd/FieldComponent.vue
+++ frontend/src/components/dd/FieldComponent.vue
@@ -14,25 +14,25 @@
       <q-btn icon="drag_indicator" flat round size="md" dense class="cursor-drag" color="grey" />
     </div>
     <div class="dd-field-content">
-      <q-badge
-        v-if="isInEditView"
-        class="dd-field-name"
-        color="grey-8"
-      >
-        <template v-if="isInEditView">
-          <span contenteditable="true"
-                @input="e=>name=e.target.innerText">{{ initialName }}</span>
-          <q-icon name="keyboard" class="q-ml-sm" v-if="field.show_in_capture" />
-          <q-icon name="print" class="q-ml-sm" v-if="field.show_in_print" />
-        </template>
-        <span v-else>{{ initialName }}</span>
-      </q-badge>
+        <div v-if="isInEditView" class="row justify-between">
+          <q-badge color="grey-8 dd-field-name " >
+            <template v-if="isInEditView">
+              <span contenteditable="true"
+                    @input="e=>name=e.target.innerText">{{ initialName }}</span>
+              <q-icon name="keyboard" class="q-ml-sm" v-if="field.show_in_capture" />
+              <q-icon name="print" class="q-ml-sm" v-if="field.show_in_print" />
+            </template>
+            <span v-else>{{ initialName }}</span>
+          </q-badge>
+        </div>
+        <btn-autocapture v-if="showGroupAutocapture" :manager="manager" :group_id="field.id" label="Auto capturar grupo"/>
+        <field-fillmap v-if="showFillmapMapper" :map_id="field.map_id || field.id" :doc_type="manager.id"/>
       <component
         v-model="value"
         :is="getComponent(field.type)"
         :class="`dd-field dd-${field.type.component}`"
-        :label="isInEditView ? field.label : field.label || field.name"
         :hint="!isInPrintView ? field.hint : null"
+        :label="isInEditView ? field.label : field.label || field.name"
         :readonly="isReadOnly"
         :group="field.id"
         :fields="fields"
@@ -40,50 +40,42 @@
         :edit_view="isInEditView"
         :capture_view="isInCaptureView"
         :print_view="isInPrintView"
+        :allowAutoCapture="allowAutoCapture"
+        :manager="manager"
       />
     </div>
-    <div class="q-ml-sm dd-field-config column items-start justify-start" v-if="isInEditView">
-      <q-btn
-        icon="settings"
-        flat
-        round
-        size="md"
-        dense
-        class="cursor-pointer"
-        color="grey"
-        @click="onShowConfigDiaog"
-      />
-      <q-btn
-        icon="delete"
-        flat
-        round
-        size="md"
-        dense
-        class="cursor-pointer"
-        color="grey"
-        @click="onDelete"
-      />
+    <div v-if="isInEditView" class="q-ml-sm dd-field-config column items-start justify-start">
+      <q-btn icon="settings" flat round size="md" dense class="cursor-pointer" color="grey" @click="onShowConfigDiaog"/>
+      <q-btn icon="delete" flat round size="md" dense class="cursor-pointer" color="grey" @click="onDelete"/>
     </div>
+    <template v-if="isInCaptureView && allowAutoCapture">
+      <btn-autocapture :manager="manager" :field_id="field.id"/>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 import {
-  DDFieldType
+  DDFieldType, EFieldComponentID
 } from "src/dynamic-documents/src/core/DDFieldType";
 import { DDField } from "src/dynamic-documents/src/core/DDField";
 import FieldConfigDialog from "src/components/dd/FieldConfig/FieldConfigDialog.vue";
 import draggable from 'vuedraggable'
 import FieldTypeSelectionDialog from "./FieldTypeSelection/FieldTypeSelectionDialog.vue";
+import FieldFillmap from './Fillmap/FieldFillmap.vue'
+import BtnAutocapture from './Fillmap/BtnAutocapture.vue'
+import { DocumentEditionManager } from "src/dynamic-documents/src/DocumentEditionManager";
 
-@Component({ components: { FieldConfigDialog, draggable } })
+@Component({ components: { FieldConfigDialog, draggable, FieldFillmap, BtnAutocapture } })
 export default class FieldComponent extends Vue {
   @Prop({ required: true }) readonly field!: DDField;
   @Prop({ type: Array, required: true }) readonly fields!: DDField[];
   @Prop({ type: Boolean, required: true }) readonly isInEditView!: boolean;
   @Prop({ type: Boolean, required: true }) readonly isInCaptureView!: boolean;
   @Prop({ type: Boolean, required: true }) readonly isInPrintView!: boolean;
+  @Prop({ type: Boolean, required: false, default: true }) readonly allowAutoCapture!:boolean;
+  @Prop({ required: false, default:()=>null }) readonly manager!:DocumentEditionManager;
   @Prop({ type: Number, required: false, default: 500 })
   readonly debounce!: number;
 
@@ -99,6 +91,18 @@ export default class FieldComponent extends Vue {
       this.initialName = value
     }
     this.ignoreNextNameChange = false
+  }
+
+  get showGroupAutocapture(){
+    return this.isGroup && this.allowAutoCapture && this.isInCaptureView
+  }
+
+  get showFillmapMapper(){
+    return !this.isGroup && this.allowAutoCapture && this.isInEditView
+  }
+
+  get isGroup(){
+    return this.field.type.component === EFieldComponentID.GROUP
   }
 
   get show () {
@@ -137,6 +141,7 @@ export default class FieldComponent extends Vue {
   }
 
   onDelete () {
+    console.log('Emiting delete', this.field)
     this.$root.$emit("f-delete", this.field);
   }
 
@@ -173,6 +178,7 @@ export default class FieldComponent extends Vue {
       index: this.field.sort_index
     });
   }
+
 }
 </script>
 
