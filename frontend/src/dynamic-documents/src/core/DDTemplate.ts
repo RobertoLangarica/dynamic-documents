e643585d86cc43edcf30bed7fd4a8657aef5c395
Roberto Langarica <roberto.langarica@gmail.com>
2021-06-05 21:12:56 -0500

    frontend/src/dynamic-documents/src/core/DDTemplate.tsfrontend/src/dynamic-documents/src/core/common/DDTemplateBaseEntity.ts: Removed since the documents and templates are now the same entity

--- frontend/src/dynamic-documents/src/core/DDTemplate.ts
+++
@@ -1,8 +0,0 @@
-import { DDTemplateBaseEntity } from "./common/DDTemplateBaseEntity";
-import { DDCategory } from "./DDCategory";
-import { Type } from "class-transformer";
-
-export class DDTemplate extends DDTemplateBaseEntity {
-    @Type(() => DDCategory)
-    categories: DDCategory[] = []
-}
