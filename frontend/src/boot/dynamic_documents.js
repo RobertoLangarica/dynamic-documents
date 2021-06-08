aed51f26c2017a9badb5c0ff52f16632a4662498
Roberto Langarica <roberto.langarica@gmail.com>
2021-06-08 15:45:24 -0500

    frontend/src/boot/dynamic_documents.js: Removing some deleted components from the boot registration

--- frontend/src/boot/dynamic_documents.js
+++ frontend/src/boot/dynamic_documents.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import FieldGroupComponent from "src/components/dd/FieldGroupComponent";
 import FieldTextEditorComponent from "src/components/dd/FieldTextEditorComponent/FieldTextEditorComponent";
-import FieldDest from "src/components/dd/Fillmap/FieldDest";
-import FieldSrc from "src/components/dd/Fillmap/FieldSrc";
 import InputNumberWrapper from "src/components/inputs/InputNumberWrapper";
 import InputDateWrapper from "src/components/inputs/InputDateWrapper";
 
 // we globally register our components
 Vue.component('field-group-component', FieldGroupComponent)
 Vue.component('field-text-editor-component', FieldTextEditorComponent)
-Vue.component('field-dest', FieldDest)
-Vue.component('field-src', FieldSrc)
 Vue.component('input-number-wrapper', InputNumberWrapper)
 Vue.component('input-date-wrapper', InputDateWrapper)
