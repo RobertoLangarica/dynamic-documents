0a04cbd62408a7fd9d56789ef71512b2c38c4e73
Roberto Langarica <roberto.langarica@gmail.com>
2021-06-13 17:02:14 -0500

    frontend/src/components/dd/Fillmap/FieldFillmap.vue: Instead of directly delete fields from a fillmap, before deletion it checks for any other document field referencing the same fillmap field. If two fields are sharing fillmap, then instead of deleting the fillmap field, a new map_id is created for the document field

--- frontend/src/components/dd/Fillmap/FieldFillmap.vue
+++ frontend/src/components/dd/Fillmap/FieldFillmap.vue
@@ -9,79 +9,98 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { v4 as uuidv4 } from 'uuid'
 import FillmapMapDialog from './FillmapMapDialog.vue'
-export default {
-    data(){
-        return { loading: false }
+
+export default Vue.extend({
+  props: {
+    manager: { type: Object, required: true },
+    field_id: { type: String, required: false, default: '' },
+    doc_type: { type: String, required: true }
+  },
+  data () {
+    return { loading: false }
+  },
+  computed: {
+    doc_field () {
+      return this.manager.fields.find(f => f.id === this.field_id)
     },
-    props:{
-        map_id:{type:String, required:true},
-        doc_type:{type:String, required:true}
+    map_id () {
+      return this.doc_field.map_id || this.doc_field.id
     },
-    computed:{
-        fillmap(){
-            // The containing fillmap
-            let fillmaps = this.$store.getters['fillmaps/byDoc'](this.doc_type)
-            return fillmaps.find(fillmap=>{
-                let field = fillmap.fields.find(f=> this.map_id === f.destination || this.map_id === f.foreign)
-                return !!field
-            })
-        },
-        field(){
-            if(!this.fillmap){
-                return undefined
-            }
-
-            return this.fillmap.fields.find(f=> this.map_id === f.destination || this.map_id === f.foreign)
-        },
-        label(){
-            if(!this.fillmap){
-                return ''
-            }
-            
-            switch(this.fillmap.source_type){
-                case 'fisica':
-                    return `Persona Fisica-${this.field.name}`;
-                case 'moral':
-                    return `Persona Moral-${this.field.name}`;
-                case 'trust':
-                    return `Fideicomiso-${this.field.name}`;
-            }
-
-            return 'Autocaptura'
-        },
-        hasMap(){
-            return !!this.fillmap
-        }
+    fillmap () {
+      // The containing fillmap
+      let fillmaps = this.$store.getters['fillmaps/byDoc'](this.doc_type)
+      return fillmaps.find(fillmap => {
+        let field = fillmap.fields.find(f => this.map_id === f.destination || this.map_id === f.foreign)
+        return !!field
+      })
     },
-    methods:{
-        onClick(){
-            // Nothing to do until the map is removed
-            if(this.hasMap)return;
+    field () {
+      if (!this.fillmap) {
+        return undefined
+      }
 
-            // Opening the dialog for the field mapping
-            this.$q.dialog({ component: FillmapMapDialog, parent: this, field_id: this.map_id, doc_type: this.doc_type })
-        },
-        async onDelete(){
-            this.loading = true
-            
-            await this.$nextTick() // To avoid the component click handler
+      return this.fillmap.fields.find(f => this.map_id === f.destination || this.map_id === f.foreign)
+    },
+    label () {
+      if (!this.fillmap) {
+        return ''
+      }
 
-            let payload = Object.assign({},this.fillmap,{fields:this.fillmap.fields.concat()})
-            let index = payload.fields.findIndex(f=> this.map_id === f.destination || this.map_id === f.foreign)
-            
-            if(index >= 0){
-                payload.fields.splice(index,1)
-            }
+      switch (this.fillmap.source_type) {
+        case 'fisica':
+          return `Persona Fisica-${this.field.name}`;
+        case 'moral':
+          return `Persona Moral-${this.field.name}`;
+        case 'trust':
+          return `Fideicomiso-${this.field.name}`;
+      }
 
-            let result = await this.$store.dispatch('fillmaps/setFillmap', payload)
-
-            if(!result.success){
-                this.$q.notify({ message: 'Ocurrió un problema al guardar los cambios', color: 'negative' })
-            }
-
-            this.loading = false
-        }
+      return 'Autocaptura'
+    },
+    hasMap () {
+      return !!this.fillmap
     }
-}
+  },
+  methods: {
+    onClick () {
+      // Nothing to do until the map is removed
+      if (this.hasMap) return;
+
+      // Opening the dialog for the field mapping
+      this.$q.dialog({ component: FillmapMapDialog, parent: this, field_id: this.map_id, doc_type: this.doc_type })
+    },
+    async onDelete () {
+      this.loading = true
+
+      // await this.$nextTick() // To avoid the component click handler
+
+      // Any other field on the document share the same map_id
+      let another = this.manager.fields.find(f => f.id !== this.field_id && (f.id === this.map_id || f.map_id === this.map_id))
+
+      if (another) {
+        // can't change the fillmap since another field is using it
+        // new map_id for this field
+        this.manager.updateField({ id: this.field_id, map_id: uuidv4() })
+      } else {
+        // delete
+        let payload = Object.assign({}, this.fillmap, { fields: this.fillmap.fields.concat() })
+        let index = payload.fields.findIndex(f => this.map_id === f.destination || this.map_id === f.foreign)
+
+        if (index >= 0) {
+          payload.fields.splice(index, 1)
+        }
+
+        let result = await this.$store.dispatch('fillmaps/setFillmap', payload)
+
+        if (!result.success) {
+          this.$q.notify({ message: 'Ocurrió un problema al guardar los cambios', color: 'negative' })
+        }
+      }
+      this.loading = false
+    }
+  }
+})
 </script>
